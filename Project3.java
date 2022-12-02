fe2c3c662c88fbd68ab559ebe878323357d73bf2
Spandan6290 <112849466+Spandan6290@users.noreply.github.com>
2022-12-01 22:18:46 -0800

    Add files via upload

---
+++ Project3.java
@@ -0,0 +1,11 @@
+public class Project3{
+public static void main (String args[]){
+int number = 15;
+if(number%2==0){
+System.out.println(number+" is a even number");
+}
+else{
+System.out.println(number+ " is a odd number");
+}
+}
+}
