cf86f2de612e2e89d3765948671d4771be42ebf0
Spandan6290 <112849466+Spandan6290@users.noreply.github.com>
2022-12-05 00:37:04 -0800

    Add files via upload

---
+++ Project7.java
@@ -0,0 +1,15 @@
+public class Project7{
+int id = 100;
+String name = "Spandan";
+String address = "Salkia Howrah";
+long phno = 7432561819L;
+static String college = "Swami Vivekananda University";
+public static void main(String[] args){
+Project7 pj = new Project7();
+System.out.println(pj.id);
+System.out.println(pj.name);
+System.out.println(pj.address);
+System.out.println(pj.college);
+System.out.println(pj.phno);
+}
+}
